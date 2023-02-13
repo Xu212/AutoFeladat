d624fbfd7657c520726da4f3f2a85b77f3b2adb9
Xu Jiyu <matexu97@gmail.com>
2023-02-13 14:07:18 +0100

    override annoticáió

--- src/modell/Auto.java
+++ src/modell/Auto.java
@@ -8,7 +8,24 @@ public class Auto extends Jarmu{
     public void kereketCserel(){
     
     }
+    @Override
     public boolean halad(){
         return true;
     }
+
+    @Override
+    public boolean tankol() {
+        return super.tankol(); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+    }
+
+    @Override
+    public void leallit() {
+        super.leallit(); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+    }
+
+    @Override
+    public void beindit() {
+        super.beindit(); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+    }
+    
 }
