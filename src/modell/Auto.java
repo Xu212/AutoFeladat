bf8139f64d1aa2845a250bee8851620edcab89dc
Xu Jiyu <matexu97@gmail.com>
2023-02-13 14:03:41 +0100

    Szerkezet elkészítése

--- src/modell/Auto.java
+++ src/modell/Auto.java
@@ -1,4 +1,14 @@
 package modell;
-public class Auto {
+public class Auto extends Jarmu{
+    private boolean defekt;
 
+    public Auto(boolean defekt) {
+        this.defekt = defekt;
+    }
+    public void kereketCserel(){
+    
+    }
+    public boolean halad(){
+        return true;
+    }
 }
--- src/modell/Jarmu.java
+++ src/modell/Jarmu.java
@@ -1,4 +1,20 @@
 package modell;
-public class Jarmu {
+public abstract class Jarmu {
+    private boolean beinditva;
+    private boolean uzemanyag;
+    private boolean megerkezett;
+    public void beindit(){
     
+    }
+    public void leallit(){
+        
+    }
+    
+    public boolean tankol(){
+        return true;
+    }
+    
+    public boolean halad(){
+        return true;
+    }
 }
